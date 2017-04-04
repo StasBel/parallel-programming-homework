d8fc3c0a625164b4e126e9785fc1e1d78c701bac
StasBel <stasbelyaev96@gmail.com>
2017-04-04 11:59:48 +0300

    Commit prerequisites

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
+#include "cl.hpp"
 
 int main() {
+    std::vector<cl::Platform> platforms;
+    std::vector<cl::Device> devices;
+    std::vector<cl::Kernel> kernels;
+
+    cl::Platform::get(&platforms);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
